3b75cd3ec9c0b0386b15263eda7d6cc5f72f4fbe
diegosdrs <your_email@example.com>
2025-01-28 15:22:43 +0100

    yes

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -6,14 +6,13 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 13:14:51 by dsindres          #+#    #+#             */
-/*   Updated: 2025/01/28 15:13:35 by marvin           ###   ########.fr       */
+/*   Updated: 2025/01/28 15:22:29 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
-"""
 int	main(int ac, char **av)
 {
 	int i = 0;
